a45bbb544eee385caca1655cefab9aba713514b1
shivani15595 <54591696+shivani15595@users.noreply.github.com>
2021-06-25 22:22:42 +0530

    Update About.test.tsx

--- src/pages/About.test.tsx
+++ src/pages/About.test.tsx
@@ -6,8 +6,18 @@ import About from './About';
 
 it('renders <About /> page', () => {
   // You should be able to verify the About page rendered correctly.
+  const {getByText} = render(<About />);
+  const headerText = getByText('About Page');
+  expect(headerText).toBeInTheDocument();
+
 });
 
 it('clicks button and fires increment counter', () => {
   // You should be able to click the increment button and verify the count.
+  const {getByText, getByTestId} = render(<About />);
+  const printerOutput = getByTestId('someTestIdForP');
+  expect(printerOutput).toHaveTextContent('Current Count: 1');
+  const buttonField = getByText('Increment');
+  userEvent.click(buttonField);
+  expect(printerOutput).toHaveTextContent('Current Count: 2');
 });
