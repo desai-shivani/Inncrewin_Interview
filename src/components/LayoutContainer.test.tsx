47ff2f9ee5ff6a55e38c8db5abc6e1b12a4f85a9
shivani15595 <54591696+shivani15595@users.noreply.github.com>
2021-06-25 22:21:52 +0530

    Update LayoutContainer.test.tsx

--- src/components/LayoutContainer.test.tsx
+++ src/components/LayoutContainer.test.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { render } from '../../test/test-utils';
 
 import LayoutContainer from './LayoutContainer';
 
 it('can browse to the about page', async () => {
   // For `LayoutContainer`, you should be able to render the layout container, followed by navigating to the About page.
+  const {getByText} = render(<LayoutContainer />);
+  const linkText = await getByText('Go to about');
+  expect(linkText).toBeTruthy();
+  userEvent.click(linkText);
+  const newPageHeader = await getByText('About Page');
+  expect(newPageHeader).toBeTruthy();
 });
