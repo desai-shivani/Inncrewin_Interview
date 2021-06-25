74e83e82a7d2ff7c9d721ac23230d8ebdcf79a7e
shivani15595 <54591696+shivani15595@users.noreply.github.com>
2021-06-25 22:23:14 +0530

    Update Home.test.tsx

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -5,4 +5,11 @@ import Home from './Home';
 
 it('renders <Home /> page', () => {
   // You should be able to show that you can verify Home rendered correctly
+  const {getByRole} = render(<Home />);
+  const linkText = getByRole('link');
+  const welcomeText = getByText('Welcome!');
+  expect(welcomeText).toBeInTheDocument();
+  expect(linkText).toBeVisible();
+  expect(linkText).toHaveTextContent('Go to about');
 });
+
